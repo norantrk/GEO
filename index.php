9150bdad1a4dd1cee29531737139bf465c0c899a
noran tarek <40693204+norantrk@users.noreply.github.com>
2023-02-01 18:26:17 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,116 @@
+<?php
+$servername = "rm-l4v670ce623mi4fxv.mysql.me-central-1.rds.aliyuncs.com";
+$username = "amb";
+$password = "No123456";
+$dbname = "demodb";
+
+//Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error){
+    die("connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+echo "hellloooo";
+
+if(isset($_POST["submit"])){
+    $latitude = $_POST['latitude'];
+    $longitude = $_POST['longitude'];
+
+    echo $latitude;
+
+$query = "UPDATE ambulances SET Ambulance_latitude = $latitude, Ambulance_longitude = $longitude WHERE id_ambulance = '001'";
+    mysqli_query($conn, $query);
+if ($conn->query($sql) == TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $query . "<br>" . $conn->error;
+}
+}
+$conn->close();
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Geolocation</title>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="map"></div>
+    <form class="myForm" action = "te.php" method = "post">
+        latitude:<input type = "text" name="latitude">
+        longtiude:<input type = "text" name="longitude"><br>
+        <input type="submit">
+    </form>
+    <script>
+      // Note: This example requires that you consent to location sharing when
+      // prompted by your browser. If you see the error "The Geolocation service
+      // failed.", it means you probably did not give permission for the browser to
+      // locate you.
+      var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 23.708824, lng: 90.364691},
+          zoom: 7
+        });
+        infoWindow = new google.maps.InfoWindow;
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            document.querySelector('.myForm input[name = "latitude"]').value = pos.lat;
+            document.querySelector('.myForm input[name = "longitude"]').value = pos.lng;
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+            map.setZoom(15);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+
+      
+      
+      
+
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBcK2Fk4blUOsBUsb1jhJ1jv-hYVaTBqZ0&callback=initMap">
+    </script>
+  </body>
+</html>
